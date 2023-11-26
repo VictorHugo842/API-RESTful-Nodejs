6c7513977629c60296d3db07e523b3deeb90305f
VictorHugo842 <victor99.santos@gmail.com.br>
2023-11-26 18:03:01 -0300

    testes unitários, sing in e busca de registro.

--- test/app_test.js
+++ test/app_test.js
@@ -15,6 +15,7 @@ describe("Testes da API Restful", function() {
       .get("/") // testa com o endpoint "/"
       .end(function(err, res) {
         expect(res).to.have.status(200); // deve retornar 200
+        console.log(res.body)
         done();
       });
   });
@@ -50,4 +51,27 @@ describe("Testes da API Restful", function() {
       });
   });
   
+    // testa rota de sign in e user
+    it("Envio de JSON para rota sign in e busca de registro com o token", function(done) {
+      chai.request(app)
+        .post("/signin")
+        .send({ email: "admin@admin.com", senha: "@admin" }) 
+        .end(function(err, res) {
+          console.log("JSON de retorno SIGN IN:", res.body);
+          
+          // tenta fazer a busca de registro utilizando o token retornado
+          if(res.status === 200){
+            const auth_token = res.body.token;
+            chai.request(app)
+              .get("/user/" + res.body.id)
+              .set("Authorization", `Bearer ${auth_token}`)
+              .end(function(err, response) {
+                console.log("passo ?")
+                console.log("JSON de retorno USER", response.body);
+                done();
+              });
+          }
+
+        });
+    });
 });
